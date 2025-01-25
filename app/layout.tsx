57f7ac25ad5175b0a82f5091baf40b5f036324e0
HumbleHominid <michael.d.fryer@gmail.com>
2025-01-25 11:04:57 -0700

    getting basic scaffolding set up for the website

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import Footer from "@/app/ui/footer";
 import { inter } from "@/app/ui/font";
+import Image from "next/image";
 
 export const metadata: Metadata = {
   title: {
@@ -18,11 +19,21 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${inter.className} antialiased`}
-      >
-        {children}
-        <Footer />
+      <body className={`${inter.className} antialiased w-full grid items-center justify-items-center`}>
+        <div className="grid grid-rows-[auto_auto_1fr_auto] items-center justify-items-center min-h-screen max-w-5xl">
+          {/* Title Image */}
+          <Image
+            src="/images/banner.png"
+            width={1500}
+            height={500}
+            alt="Arcadia Banner Image"
+          />
+          {children}
+          {/* Footer */}
+          <div className="row-start-4 w-full max-w-5xl p-2">
+            <Footer />
+          </div>
+        </div>
       </body>
     </html>
   );
