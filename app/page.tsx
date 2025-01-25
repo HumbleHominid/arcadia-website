73453b249472c7b407022b9ae09db346acd54e57
HumbleHominid <michael.d.fryer@gmail.com>
2025-01-25 12:37:13 -0700

    adding side-bar stuff for site navigation

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,28 @@
+import CollapseableList from "./ui/collapseable-list";
+
 export default function Home() {
+  // TODO: Get from skeleton data then DB
+  const members = [
+    {src: "/images/user.svg", url: "/HumbleHominid", text: "HumbleHominid"},
+    {src: "/images/user.svg", url: "/Zoontii", text: "Zoontii"},
+  ];
+  const community = [
+    {src: "/images/twitter_icon.png", url: "https://x.com/Arcadia_SMP", text: "Twitter/X"},
+    {src: "/images/bluesky_icon.svg", url: "https://bsky.app/profile/arcadiasmp.bsky.social", text: "Bluesky"},
+    {src: "/images/discord-mark-blue.svg", url: "https://discord.gg/btwNnKtWQu", text: "Discord"},
+  ];
+  const server = [
+    {src: "/images/arrow-down-tray.svg", url: "https://www.mediafire.com/file/an45poqm8p6ldc3/arcadia-smp-season1-v1.zip/file", text: "Season 1 World Download"},
+    {src: "/images/moddermore-positive.png", url: "https://moddermore.net/list/8a52816c90", text: "Server Mods"},
+  ];
   return (
-    <div>
-      <main>
-      </main>
-    </div>
+    <article className="flex md:flex-row flex-col gap-4">
+      {/* Member and Server Info */}
+      <div className="flex flex-col">
+        <CollapseableList data={members} title="Members" />
+        <CollapseableList data={community} title="Community" />
+        <CollapseableList data={server} title="Server Stuff" />
+      </div>
+    </article>
   );
 }
