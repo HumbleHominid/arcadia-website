da544420866bb2fe90ca1d0d6bfcd8e207f968c2
HumbleHominid <michael.d.fryer@gmail.com>
2025-01-25 23:31:47 -0700

    temporarily reducing the video list skeleton length so there is less popping (we don't have enough videos in the db to prevent this at the moment)

--- app/ui/skeletons/video-list-skeleton.tsx
+++ app/ui/skeletons/video-list-skeleton.tsx
@@ -3,7 +3,7 @@ import VideoSkeleton from "@/app/ui/skeletons/video-skeleton"
 export default function VideoListSkeleton() {
 	return (
 			<div className="flex flex-col w-full divide-y px-2">
-				{[...Array(10).keys()].map((val) => {
+				{[...Array(4).keys()].map((val) => {
 					return (
 						<VideoSkeleton key={val} />
 					)})}
