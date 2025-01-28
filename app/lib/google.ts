98ab049c1eb6e0b6c35bbf4cecba68b9bdd2fe82
HumbleHominid <michael.d.fryer@gmail.com>
2025-01-27 17:12:17 -0700

    adding better exception handling for google lib

--- app/lib/google.ts
+++ app/lib/google.ts
@@ -1,22 +1,32 @@
 import { google } from "googleapis";
 
 export async function getAuth() {
-	const scopes = ['https://www.googleapis.com/auth/youtube.readonly'];
-
 	const auth = new google.auth.JWT({
 		email: process.env.GOOGLE_CLIENT_EMAIL,
 		key: process.env.GOOGLE_PRIVATE_KEY,
-		scopes: scopes
+		scopes: ['https://www.googleapis.com/auth/youtube.readonly']
 	});
 
-	await auth.authorize();
-	return auth;
+	try {
+		await auth.authorize();
+		return auth;
+	}
+	catch (e) {
+		console.error('getAuth Failed');
+		throw e;
+	}
 }
 
 export async function getYouTube() {
-	const auth = await getAuth();
-	return google.youtube({
-		version: "v3",
-		auth,
-	});
+	try {
+		const auth = await getAuth();
+		return google.youtube({
+			version: "v3",
+			auth,
+		});
+	}
+	catch (e) {
+		console.error('getYouTube Failed');
+		throw e;
+	}
 }
