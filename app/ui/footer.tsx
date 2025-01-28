a53d74d79fbd32408827a0ed65dd2a453bfc858a
HumbleHominid <michael.d.fryer@gmail.com>
2025-01-27 22:27:29 -0700

    fixing the link in the footer

--- app/ui/footer.tsx
+++ app/ui/footer.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Footer() {
 	return (
 		<footer className="rounded-sm px-4 py-2 bg-slate-50 text-slate-800 drop-shadow-sm text-center">
-			Website written and maintained by <Link href="/HumbleHominid" className="hover:underline underline-offset-2 decoration-1">HumbleHominid</Link>.
+			Website written and maintained by <Link href="/member/@humblehominid" className="hover:underline underline-offset-2 decoration-1">HumbleHominid</Link>.
 		</footer>
 	)
 }
