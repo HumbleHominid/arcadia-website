7a4e486c70fc174af9ef0488ec5a551b9e96a9cf
HumbleHominid <michael.d.fryer@gmail.com>
2025-01-27 19:05:38 -0700

    adding duration to the videos

--- app/ui/video.tsx
+++ app/ui/video.tsx
@@ -9,6 +9,23 @@ export default function Video({
 	data: VideoData
 }) {
 	const posted = formatDistance(new Date(data.publish_date), new Date(), { addSuffix: true });
+	let duration = data.duration
+		.replace('PT', '')
+		.replace('H', ':')
+		.replace('M', ':')
+		.replace('S', '')
+		.split(':')
+		.map((t) => {
+			const s = '00' + t;
+			return s.substring(s.length - 2);
+		})
+		.join(':')
+		.replace(/^0+/,'');
+		// If the video is less than a minute, prepend a '0:'
+		if (duration.split(':').length === 1) {
+			duration = '0:' + duration;
+		}
+
 	return (
 		<div className="grid grid-cols-[auto_1fr] py-2 gap-4">
 			<Link
@@ -16,7 +33,7 @@ export default function Video({
 				target="_blank"
 				rel="noopener noreferrer"
 			>
-				<div className={"w-[160px] md:w-[200px] md: flex items-center overflow-hidden"}
+				<div className={"w-[160px] md:w-[200px] overflow-hidden relative"}
 				>
 					<Image
 						src={`https://i.ytimg.com/vi/${data.video_id}/mqdefault.jpg`}
@@ -25,6 +42,10 @@ export default function Video({
 						height={144}
 						className="align-middle overflow-hidden"
 					/>
+					{/* The duration stamp */}
+					<span className="bg-slate-800 bg-opacity-60 text-white absolute bottom-0 right-0 mr-1 mb-1 px-1 text-xs">
+						{duration}
+					</span>
 				</div>
 			</Link>
 			<div>
