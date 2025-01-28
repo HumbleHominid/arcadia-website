3dab69bc39151a15ffcc4aa07776da9ac25d5215
HumbleHominid <michael.d.fryer@gmail.com>
2025-01-28 13:51:33 -0700

    adjusting footer font size for mobile

--- app/ui/footer.tsx
+++ app/ui/footer.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function Footer() {
 	return (
-		<footer className="rounded-sm px-4 py-2 bg-slate-50 text-slate-800 drop-shadow-sm text-center">
+		<footer className="rounded-sm px-4 py-2 bg-slate-50 text-slate-800 drop-shadow-sm text-center text-xs md:text-base">
 			Website written and maintained by <Link href="/member/@humblehominid" className="hover:underline underline-offset-2 decoration-1">HumbleHominid</Link>.
 		</footer>
 	)
