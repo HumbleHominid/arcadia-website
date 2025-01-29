82b2e35e8ef5d6f515212dbfbac8907ca5127c4d
HumbleHominid <michael.d.fryer@gmail.com>
2025-01-28 18:10:38 -0700

    fixing title being off-center on firefox

--- app/layout.tsx
+++ app/layout.tsx
@@ -73,17 +73,24 @@ export default function RootLayout({
           alt="Arcadia Background Image"
           className="sticky top-0 -z-10 h-screen min-w-full object-cover row-span-full col-span-full"
         />
-        <main className="grid grid-rows-[auto_auto_1fr_auto] items-center justify-items-center min-h-screen max-w-5xl row-span-full col-span-full mx-0 my-auto">
+        <main className="grid grid-rows-[auto_auto_1fr_auto] items-center min-h-screen max-w-5xl row-span-full col-span-full mx-0 my-auto">
           {/* Title Image */}
-          <Link href="/">
-            <Image
-              src="/images/banner.png"
-              width={1500}
-              height={500}
-              alt="Arcadia Banner Image"
-              className="w-full md:w-8/12 place-self-center"
-            />
-          </Link>
+          <div
+            className="w-full md:w-8/12 place-self-center"
+          >
+            <Link
+              href="/"
+              className=""
+            >
+              <Image
+                src="/images/banner.png"
+                width={1500}
+                height={500}
+                alt="Arcadia Banner Image"
+                className=""
+              />
+            </Link>
+          </div>
           {children}
           {/* Footer */}
           <div className="row-start-4 w-full max-w-5xl p-2">
