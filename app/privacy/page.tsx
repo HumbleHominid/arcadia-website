7d8cd16bef63a23e43b31943d985148b11ed1207
HumbleHominid <michael.d.fryer@gmail.com>
2025-01-29 12:43:19 -0700

    Fixing linter errors

--- app/privacy/page.tsx
+++ app/privacy/page.tsx
@@ -6,7 +6,7 @@ export const metadata: Metadata = {
 	title: 'Privacy'
 }
 
-export default function() {
+export default function Page() {
 	let hostname: string = '';
 	switch (process.env.NODE_ENV) {
 		case "production":
@@ -26,7 +26,7 @@ export default function() {
 			<div className="w-full bg-white rounded-sm drop-shadow-sm md:drop-shadow-xl p-4">
 				<h1 className="text-4xl md:text-6xl font-thin mb-4">Privacy Policy</h1>
 				<h2 className="text-xl font-light mb-2">YouTube Client API</h2>
-				<p className="text-sm md:text-base">{hostname} uses a YouTube API Client that uses YouTube API Services. By using {hostname} users are agreeing to be bound by the YouTube Terms of Service. The API Client is internal and does not use visitor information. The API Client retrieves publicly accessible data about videos and stores this data in it's database. This information is used to display video information on this website and is not shared externally.</p>
+				<p className="text-sm md:text-base">{hostname} uses a YouTube API Client that uses YouTube API Services. By using {hostname} users are agreeing to be bound by the YouTube Terms of Service. The API Client is internal and does not use visitor information. The API Client retrieves publicly accessible data about videos and stores this data in it&apos;s database. This information is used to display video information on this website and is not shared externally.</p>
 				<p>
 					<Link
 						href="https://www.youtube.com/t/terms"
