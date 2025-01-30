a4e5ed66e3973bc7519cb16e23d7f649f86b5afc
HumbleHominid <michael.d.fryer@gmail.com>
2025-01-29 20:22:08 -0700

    updating google api to be using a workload pool instead of directly using the service account

--- app/lib/google.ts
+++ app/lib/google.ts
@@ -1,28 +1,36 @@
 import { google } from "googleapis";
+import { ExternalAccountClient } from "google-auth-library";
+import { getVercelOidcToken } from "@vercel/functions/oidc";
 
-export async function getAuth() {
-	const auth = new google.auth.JWT({
-		email: process.env.GOOGLE_CLIENT_EMAIL,
-		key: process.env.GOOGLE_PRIVATE_KEY,
+export function getYouTube() {
+	const GCP_PROJECT_NUMBER = process.env.GCP_PROJECT_NUMBER;
+	const GCP_SERVICE_ACCOUNT_EMAIL = process.env.GCP_SERVICE_ACCOUNT_EMAIL;
+	const GCP_WORKLOAD_IDENTITY_POOL_ID = process.env.GCP_WORKLOAD_IDENTITY_POOL_ID;
+	const GCP_WORKLOAD_IDENTITY_POOL_PROVIDER_ID = process.env.GCP_WORKLOAD_IDENTITY_POOL_PROVIDER_ID;
+
+	const authClient = ExternalAccountClient.fromJSON({
+		type: 'external_account',
+		audience: `//iam.googleapis.com/projects/${GCP_PROJECT_NUMBER}/locations/global/workloadIdentityPools/${GCP_WORKLOAD_IDENTITY_POOL_ID}/providers/${GCP_WORKLOAD_IDENTITY_POOL_PROVIDER_ID}`,
+		subject_token_type: 'urn:ietf:params:oauth:token-type:jwt',
+		token_url: 'https://sts.googleapis.com/v1/token',
+		service_account_impersonation_url: `https://iamcredentials.googleapis.com/v1/projects/-/serviceAccounts/${GCP_SERVICE_ACCOUNT_EMAIL}:generateAccessToken`,
+		subject_token_supplier: {
+			// Use the Vercel OIDC token as the subject token
+			getSubjectToken: getVercelOidcToken,
+		},
 		scopes: ['https://www.googleapis.com/auth/youtube.readonly']
 	});
 
-	try {
-		await auth.authorize();
-		return auth;
+	if (!authClient) {
+		const err = 'Failed to create an external account client with vercel OIDC';
+		console.error(err);
+		throw new Error(err);
 	}
-	catch (e) {
-		console.error('getAuth Failed', e);
-		throw e;
-	}
-}
 
-export async function getYouTube() {
 	try {
-		const auth = await getAuth();
 		return google.youtube({
-			version: "v3",
-			auth,
+			version: 'v3',
+			auth: authClient,
 		});
 	}
 	catch (e) {
