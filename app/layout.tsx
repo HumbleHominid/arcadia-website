eb54f659f69e9b7a3c14228d4d4e7a4ce5754320
HumbleHominid <michael.d.fryer@gmail.com>
2025-02-11 09:18:41 -0700

    fixing open graph url

--- app/layout.tsx
+++ app/layout.tsx
@@ -44,7 +44,7 @@ export const metadata: Metadata = {
     title: 'Arcadia',
     description: 'The official website for Arcadia!',
     siteName: 'Arcadia',
-    url: '/',
+    url: process.env.VERCEL_PROJECT_PRODUCTION_URL ? process.env.VERCEL_PROJECT_PRODUCTION_URL : 'localhost:3000',
     images: [
       {
         url: '/images/twitter.png',
