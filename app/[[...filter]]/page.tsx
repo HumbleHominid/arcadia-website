2f1ee8d03cb3b82e429648b959188bd3eaf26465
Michael Fryer <michael.d.fryer@gmail.com>
2025-08-03 16:33:25 +0200

    adding an announcements frontend

--- app/[[...filter]]/page.tsx
+++ app/[[...filter]]/page.tsx
@@ -1,4 +1,9 @@
-import { fetchMembers, fetchAllVideos, fetchArcadiaVideos, fetchLatestVideos } from "@/app/lib/data";
+import {
+  fetchMembers,
+  fetchAllVideos,
+  fetchArcadiaVideos,
+  fetchLatestVideos,
+} from "@/app/lib/data";
 import CollapseableList from "@/app/ui/collapseable-list/collapseable-list";
 import VideoList from "@/app/ui/video-list";
 import PageLayout from "@/app/ui/page-layout";
@@ -10,29 +15,31 @@ import { Metadata } from "next";
 import VideoFilter from "@/app/ui/video-filter/video-filter";
 import { unstable_cache } from "next/cache";
 import { notFound } from "next/navigation";
+import Announcement from "@/app/ui/announcement";
 
 type Props = {
-  params: Promise<{filter: FilterType}>;
-}
+  params: Promise<{ filter: FilterType }>;
+};
 
 export const metadata: Metadata = {
-  title: 'Home'
-}
+  title: "Home",
+};
 
 export default async function Page({ params }: Props) {
   const resolvedParams = await params;
-  const latest = 'Latest'
-  const filter = resolvedParams.filter === undefined ? 'Latest' : resolvedParams.filter[0];
-  const validFilters: string[] = [ FilterType.All, FilterType.Arcadia, latest ];
+  const latest = "Latest";
+  const filter =
+    resolvedParams.filter === undefined ? "Latest" : resolvedParams.filter[0];
+  const validFilters: string[] = [FilterType.All, FilterType.Arcadia, latest];
   if (!validFilters.includes(filter)) notFound();
 
   const getCachedMembers = unstable_cache(
     async () => {
       return await fetchMembers();
     },
-    ['members'],
-    { revalidate: 24 * 60 * 60, tags: ['members'] } // Make the member's cache stale after 24h
-  )
+    ["members"],
+    { revalidate: 24 * 60 * 60, tags: ["members"] } // Make the member's cache stale after 24h
+  );
   const getCachedVideos = unstable_cache(
     async (filter: string) => {
       switch (filter) {
@@ -48,40 +55,67 @@ export default async function Page({ params }: Props) {
     },
     [`${filter}-videos`],
     { revalidate: 10 * 60, tags: [`${filter}-videos`] }
-  )
+  );
 
   // Try to update the DB with new videos
   const members = getCachedMembers();
   const videos = getCachedVideos(filter);
   const community = [
-    {src: "/icons/twitter_icon.png", url: "https://x.com/Arcadia_SMP", text: "Arcadia_SMP"},
-    {src: "/icons/bluesky_icon.svg", url: "https://bsky.app/profile/arcadiasmp.bsky.social", text: "arcadiasmp.bsky.social"},
-    {src: "/icons/discord-mark-blue.svg", url: "https://discord.gg/btwNnKtWQu", text: "The Arcadians"},
+    {
+      src: "/icons/twitter_icon.png",
+      url: "https://x.com/Arcadia_SMP",
+      text: "Arcadia_SMP",
+    },
+    {
+      src: "/icons/bluesky_icon.svg",
+      url: "https://bsky.app/profile/arcadiasmp.bsky.social",
+      text: "arcadiasmp.bsky.social",
+    },
+    {
+      src: "/icons/discord-mark-blue.svg",
+      url: "https://discord.gg/btwNnKtWQu",
+      text: "The Arcadians",
+    },
   ];
   const server = [
-    {src: "/icons/arrow-down-tray.svg", url: "https://www.mediafire.com/file/an45poqm8p6ldc3/arcadia-smp-season1-v1.zip/file", text: "Season 1 World Download"},
-    {src: "/icons/arrow-down-tray.svg", url: "https://www.mediafire.com/file/rednhdarehr7ftf/arcadia-season2-world-v2.zip/file", text: "Season 2 World Download"},
-    {src: "/icons/moddermore-positive.png", url: "https://moddermore.net/list/8a52816c90", text: "Server Mods"},
+    {
+      src: "/icons/arrow-down-tray.svg",
+      url: "https://www.mediafire.com/file/an45poqm8p6ldc3/arcadia-smp-season1-v1.zip/file",
+      text: "Season 1 World Download",
+    },
+    {
+      src: "/icons/arrow-down-tray.svg",
+      url: "https://www.mediafire.com/file/rednhdarehr7ftf/arcadia-season2-world-v2.zip/file",
+      text: "Season 2 World Download",
+    },
+    {
+      src: "/icons/moddermore-positive.png",
+      url: "https://moddermore.net/list/8a52816c90",
+      text: "Server Mods",
+    },
   ];
   return (
-    <PageLayout>
-      {/* Member and Server Info */}
-      <div className="flex flex-col gap-1 w-full md:w-4/12">
-        <Suspense fallback={<CollapseableList title="Members" />}>
-          <MembersList members={members}/>
-        </Suspense>
-        <CollapseableList data={community} title="Community" />
-        <CollapseableList data={server} title="Server Stuff" />
-      </div>
-      {/* Video section */}
-      <div className="w-full h-min flex flex-col items-center bg-white rounded-sm drop-shadow-sm md:drop-shadow-xl text-lg">
-        {/* Need a context switcher */}
-        <VideoFilter />
-        {/* List of videos */}
-        <Suspense fallback={<VideoListSkeleton />}>
-          <VideoList videos={videos} />
-        </Suspense>
-      </div>
-    </PageLayout>
+    <div className="flex flex-col gap-4">
+      <Announcement />
+      <PageLayout>
+        {/* Member and Server Info */}
+        <div className="flex flex-col gap-1 w-full md:w-4/12">
+          <Suspense fallback={<CollapseableList title="Members" />}>
+            <MembersList members={members} />
+          </Suspense>
+          <CollapseableList data={community} title="Community" />
+          <CollapseableList data={server} title="Server Stuff" />
+        </div>
+        {/* Video section */}
+        <div className="w-full h-min flex flex-col items-center bg-white rounded-sm drop-shadow-sm md:drop-shadow-xl text-lg">
+          {/* Need a context switcher */}
+          <VideoFilter />
+          {/* List of videos */}
+          <Suspense fallback={<VideoListSkeleton />}>
+            <VideoList videos={videos} />
+          </Suspense>
+        </div>
+      </PageLayout>
+    </div>
   );
 }
---
+++ app/ui/announcement.tsx
@@ -0,0 +1,41 @@
+import Image from "next/image";
+import Accordian from "@/app/ui/accordian";
+
+type AnnouncementData = {
+  title: string;
+  description: string;
+  imgSrc: string;
+  endDate: Date;
+};
+
+export default function Announcement() {
+  // TODO: Fetch announcements from the DB so the codebase doesn't have to change every
+  //  time there is a new announcement.
+  const data: AnnouncementData = {
+    title: "Arcadia Stream Weekend",
+    description:
+      "We are officially organizing our first ever Charity Stream Weekend! Taking place August 16th and 17th, we are going to be raising money for the wonderful charity of Gamer's Outreach. Keep your eyes peeled closer to the date for the exact times and the streaming schedule - we are going to be running some mini events and interacting a whole bunch with the community! If there's anything specific you'd like to see us do on the weekend, including rewards for fundraising goals - drop a suggestion in the ⁠💡-suggestions chat of our Discord. Hope to see you all there and hope everyone is hyped! ",
+    imgSrc: "/images/charity_stream_2025.png",
+    endDate: new Date(2025, 8, 18),
+  };
+  if (new Date() > data.endDate) return <></>;
+
+  return (
+    // TODO: Only display the announcement if the event isn't expired
+    <div className="w-full px-2">
+      <Accordian title="Announcement" isExpandedDefault={true}>
+        <div className="grid md:grid-cols-[1fr_4fr] md:grid-rows-[auto_1fr] grid-rows-[auto_auto_auto] gap-x-4 p-1 md:p-2 bg-white rounded-sm drop-shadow-sm md:drop-shadow-md">
+          <Image
+            src={data.imgSrc}
+            alt={`${data.title} promo image`}
+            width={1587}
+            height={2245}
+            className="mr-1 md:mr-1.5 h-auto w-96 self-center md:row-span-2"
+          />
+          <h1 className="text-4xl md:text-6xl font-thin mb-4">{data.title}</h1>
+          <p className="text-base md:text-lg">{data.description}</p>
+        </div>
+      </Accordian>
+    </div>
+  );
+}
